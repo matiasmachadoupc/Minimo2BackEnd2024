4bbe149ee1c79822bee8bd1cb86025a75367bd0d
Matias <matias.machado@estudiantat.upc.edu>
2024-12-01 10:06:13 +0100

    Implementada la funcion de comprar y  el inventario, falta ajustar el mostrar el inventario del usuario en la web

--- src/main/java/edu/upc/dsa/UserManager.java
+++ src/main/java/edu/upc/dsa/UserManager.java
@@ -1,24 +1,19 @@
 package edu.upc.dsa;
 
-import edu.upc.dsa.exceptions.UserNotFoundException;
-    import edu.upc.dsa.models.User;
+import edu.upc.dsa.models.Inventory;
+import edu.upc.dsa.models.User;
 
 import java.util.List;
 
 public interface UserManager {
-
-
     public User addUser(String username, String password, String isAdmin, String fullName, String email, int age, String profilePicture, int coins);
     public User addUser(String username, String password, String isAdmin);
     public User addUsers(User user);
     public User getUser(String id);
     public User getUserProfileByUsername(String username);
-
-
     public List<User> findAll();
     public void deleteUser(String id);
     public User updateUser(User t);
-
     public void clear();
     public int size();
     public User getUserByUsername(String username);
